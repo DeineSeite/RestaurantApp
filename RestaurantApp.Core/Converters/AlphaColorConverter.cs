f0b37eccf67f23d6ee4b6750133e95fcfa57293f
Vladyslav Kostiushko <vladik1996@live.ru>
2017-01-27 23:36:15 +0100

    ContactPage with Map

---
+++ RestaurantApp.Core/Converters/AlphaColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestaurantApp.Core.Converters
+{
+    public class AlphaColorConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+           
+            if (targetType == typeof (Color))
+            {
+                var color = (Color) value;
+                
+                double alpha = double.Parse(parameter.ToString());
+                color.MultiplyAlpha(alpha);
+                return color;
+
+            }
+            return new InvalidOperationException("Cannot convert type" + targetType.Name + "to Color");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ RestaurantApp.Core/Converters/StringCaseConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestaurantApp.Core.Converters
+{
+    public class StringCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            string param = System.Convert.ToString(parameter) ?? "UPPER";
+
+            switch (param.ToUpper())
+            {
+                case "UPPER":
+                    return ((string) value).ToUpper();
+                case "LOWER":
+                    return ((string) value).ToLower();
+                default:
+                    return ((string) value);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
