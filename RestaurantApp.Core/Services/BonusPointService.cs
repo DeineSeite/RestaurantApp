98a523d4beb3782ce7df1a4ea85de5d68302aa67
Vladyslav Kostiushko <vladik1996@live.ru>
2017-02-12 12:17:20 +0100

    Xamarin.Forms.2.3.4.22-nightly for Azure Center

---
+++ RestaurantApp.Core/Services/BonusPointService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Core.Services
+{
+    class BonusPointService
+    {
+    }
+}
---
+++ RestaurantApp.Data/Models/AppInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Data.Models
+{
+   public class AppInfo
+    {
+        public  string OS { get; set; }
+        public  string OSVersion { get; set; }
+        public  string Language { get; set; }
+        public  string AppVersion { get; set; }
+        public string UniqueId { get; set; }
+    }
+}
